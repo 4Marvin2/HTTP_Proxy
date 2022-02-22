f63879eb08cab04f39a833bbd177e77fa42d6e27
Mikhail Popov <mikh.popov@vk.team>
2022-02-23 00:16:58 +0300

    finally ..... reworked via http

--- httpProxy/httpProxy.go
+++ httpProxy/httpProxy.go
@@ -1,6 +1,7 @@
 package httpProxy
 
 import (
+	"bufio"
 	"crypto/tls"
 	"errors"
 	"fmt"
@@ -8,238 +9,177 @@ import (
 	"log"
 	"math"
 	"math/rand"
-	"net"
+	"net/http"
+	"net/http/httputil"
 	"os/exec"
 	"strconv"
 	"strings"
-	"time"
 )
 
 const (
 	Host = "127.0.0.1"
-	Port = "8080"
-)
-
-const BufferSuze = 4096
-const (
-	SlashRCode = 13
-	SlashNCode = 10
+	Port = ":8080"
 )
 
 func Run() {
-	ln, err := net.Listen("tcp", ":"+Port)
-	if err != nil {
-		log.Fatalf("Unable to start listener, %v\n", err)
-	}
-	defer ln.Close()
-	log.Printf("Start listening on port %s\n", Port)
-	for {
-		c, err := ln.Accept()
-		if err != nil {
-			log.Printf("Accept connection err: %v\n", err)
-		}
-		go handleConnection(c)
+	server := http.Server{
+		Addr: Port,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodConnect {
+				handleHTTPS(w, r)
+			} else {
+				handleHTTP(w, r)
+			}
+		}),
 	}
 
-	// cer, err := tls.LoadX509KeyPair("certs/127.0.0.1.crt", "cert.key")
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-
-	// config := &tls.Config{
-	// 	Certificates:       []tls.Certificate{cer},
-	// 	InsecureSkipVerify: true,
-	// }
-	// ln, err := tls.Listen("tcp", ":8080", config)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-	// defer ln.Close()
-
-	// for {
-	// 	conn, err := ln.Accept()
-	// 	if err != nil {
-	// 		log.Println(err)
-	// 		continue
-	// 	}
-	// 	go handleConnection(conn)
-	// }
+	log.Fatal(server.ListenAndServe())
 }
 
-func handleConnection(c net.Conn) {
-	log.Printf("Serving %s\n", c.RemoteAddr().String())
+func handleHTTP(w http.ResponseWriter, r *http.Request) {
+	for key := range r.Header {
+		if key == "Proxy-Connection" {
+			r.Header.Del(key)
+		}
+	}
 
-	request, err := readResponse(c)
+	resp, err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
-		log.Printf("Cannot read data from initiator: %v\n", err)
-		c.Close()
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
 		return
 	}
-	fmt.Println(string(request))
+	defer resp.Body.Close()
 
-	parsedReq := strings.Split(string(request), "\n")
-	firstLine := strings.Split(parsedReq[0], " ")
-	method := firstLine[0]
-	url := firstLine[1]
-	if method == "CONNECT" {
-		parsedUrl := strings.Split(url, ":")
-		err := handleSecureConn(c, parsedUrl[0])
-		if err != nil {
-			log.Printf("Cannot establishe secure connection: %v", err)
-			c.Close()
-			return
+	for key, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
 		}
-		request, err = readResponse(c)
-		if err != nil {
-			log.Printf("Cannot read data from initiator after established secure conn: %v\n", err)
-			c.Close()
-			return
-		}
-		fmt.Println(string(request))
-		// newReq := strings.Replace(string(request), "1.1", "2", 1)
-		// fmt.Println(newReq)
-		cer, err := tls.LoadX509KeyPair(parsedUrl[0]+".crt", "cert.key")
-		rConn, err := tls.Dial("tcp", url, &tls.Config{Certificates: []tls.Certificate{cer}})
-		err = rConn.Handshake()
-		fmt.Println(err)
-		// rConn, err := net.Dial("tcp", "mail.ru:443")
-		// request := "GET / HTTP/1.1\r\nHost: mail.ru\r\nUser-Agent: Go-Proxy\r\nAccept: */*\r\n\r\n"
-		if _, err := rConn.Write([]byte(request)); err != nil {
-			log.Printf("Cannot write to remote host: %v\n", err)
-			c.Close()
-			rConn.Close()
-			return
-		}
-		response, err := readResponse(rConn)
-		if err != nil {
-			log.Printf("Cannot read data from remote host: %v\n", err)
-			c.Close()
-			rConn.Close()
-			return
-		}
-		rConn.Close()
-		fmt.Println(string(response))
-
-		fmt.Println(4)
-		if _, err := c.Write(response); err != nil {
-			log.Printf("Cannot write to initiator: %v\n", err)
-			c.Close()
-			return
-		}
-		fmt.Println(err)
-		c.Close()
-		return
 	}
-
-	hostAndPort, newReq := preparationForProxying(c, request)
-
-	rConn, err := net.Dial("tcp", hostAndPort)
+	w.WriteHeader(resp.StatusCode)
+	_, err = io.Copy(w, resp.Body)
 	if err != nil {
-		log.Printf("Cannot connect to remote host: %v\n", err)
-		c.Close()
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer rConn.Close()
-
-	if _, err := rConn.Write(newReq); err != nil {
-		log.Printf("Cannot write to remote host: %v\n", err)
-		c.Close()
-		rConn.Close()
-		return
-	}
-
-	time.Sleep(100 * time.Millisecond)
-
-	response, err := readResponse(rConn)
-	if err != nil {
-		log.Printf("Cannot read data from remote host: %v\n", err)
-		c.Close()
-		rConn.Close()
-		return
-	}
-	rConn.Close()
-
-	if _, err := c.Write(response); err != nil {
-		log.Printf("Cannot write to initiator: %v\n", err)
-		c.Close()
-		return
-	}
-	c.Close()
 }
 
-func readResponse(c net.Conn) ([]byte, error) {
-	var request []byte
-	contentLength := 0
-	var err error
-	var n int
-	for err != io.EOF {
-		buffer := make([]byte, BufferSuze)
-		n, err = c.Read(buffer)
-		if err != nil && err != io.EOF {
-			return request, err
-		}
-		contentLength += n
-		request = append(request, buffer...)
-		request = request[:contentLength]
-		if n < BufferSuze {
-			break
-		}
+func handleHTTPS(w http.ResponseWriter, r *http.Request) {
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		log.Println("Hijacking not supported")
+		http.Error(w, "Hijacking not supported", http.StatusInternalServerError)
+		return
 	}
 
-	return request, nil
+	localConn, _, err := hijacker.Hijack()
+	if err != nil {
+		log.Printf("hijacking error: %v", err)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+	}
+
+	_, err = localConn.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
+	if err != nil {
+		log.Printf("handshaking failed: %v", err)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		localConn.Close()
+		return
+	}
+	defer localConn.Close()
+
+	host := strings.Split(r.Host, ":")[0]
+
+	tlsConfig, err := generateTLSConfig(host, r.URL.Scheme)
+	if err != nil {
+		log.Printf("error getting cert: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tlsLocalConn := tls.Server(localConn, &tlsConfig)
+	err = tlsLocalConn.Handshake()
+	if err != nil {
+		tlsLocalConn.Close()
+		log.Printf("handshaking failed: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer tlsLocalConn.Close()
+
+	remoteConn, err := tls.Dial("tcp", r.URL.Host, &tlsConfig)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer remoteConn.Close()
+
+	reader := bufio.NewReader(tlsLocalConn)
+	request, err := http.ReadRequest(reader)
+	if err != nil {
+		log.Printf("error getting request: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	requestByte, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		log.Printf("failed to dump request: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = remoteConn.Write(requestByte)
+	if err != nil {
+		log.Printf("failed to write request: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	serverReader := bufio.NewReader(remoteConn)
+	response, err := http.ReadResponse(serverReader, request)
+	if err != nil {
+		log.Printf("failed to read response: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rawResponse, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		log.Printf("failed to dump response: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = tlsLocalConn.Write(rawResponse)
+	if err != nil {
+		log.Printf("fail to write response: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
-func preparationForProxying(c net.Conn, request []byte) (string, []byte) {
-	parsedReq := strings.Split(string(request), "\n")
-	firstLine := strings.Split(parsedReq[0], " ")
-	url := firstLine[1]
-
-	parseURL := strings.Split(firstLine[1], "/")
-	protocol := parseURL[0]
-	hostAndPort := parseURL[2]
-	if !strings.Contains(hostAndPort, ":") {
-		if strings.Contains(protocol, "https") {
-			hostAndPort += ":443"
-		} else if strings.Contains(protocol, "http") {
-			hostAndPort += ":80"
-		}
-	}
-	iter := 3
-	var path string
-	for iter < len(parseURL) {
-		path += "/" + parseURL[iter]
-		iter++
-	}
-
-	newReq := strings.Replace(string(request), url, path, 1)
-
-	proxyConnHeaderStartIndex := strings.Index(newReq, "Proxy-Connection")
-	proxyConnHeaderEndIndex := strings.Index(newReq[proxyConnHeaderStartIndex:], "\n") + proxyConnHeaderStartIndex
-	newReq = newReq[:proxyConnHeaderStartIndex] + newReq[proxyConnHeaderEndIndex+1:]
-
-	return hostAndPort, []byte(newReq)
-}
-
-func handleSecureConn(c net.Conn, host string) error {
+func generateTLSConfig(host string, URL string) (tls.Config, error) {
 	cmd := exec.Command("/bin/sh", "./scripts/gen_cert.sh", host, strconv.Itoa(rand.Intn(math.MaxInt32)))
 
 	err := cmd.Start()
 	if err != nil {
-		return errors.New(fmt.Sprintf("Start create cert file script error: %v\n", err))
+		return tls.Config{}, errors.New(fmt.Sprintf("Start create cert file script error: %v\n", err))
 	}
 
 	err = cmd.Wait()
 	if err != nil {
-		return errors.New(fmt.Sprintf("Wait create cert file script error: %v\n", err))
+		return tls.Config{}, errors.New(fmt.Sprintf("Wait create cert file script error: %v\n", err))
 	}
 
-	resp := "HTTP/1.0 200 Connection established\r\n\r\n" +
-		"Proxy-agent: Golang-Proxy\r\n\r\n\r\n"
-	if _, err := c.Write([]byte(resp)); err != nil {
-		return errors.New(fmt.Sprintf("Cannot write connect response to initiator: %v\n", err))
+	tlsCert, err := tls.LoadX509KeyPair("certs/"+host+".crt", "cert.key")
+	if err != nil {
+		log.Println("error loading pair", err)
+		return tls.Config{}, err
 	}
 
-	return nil
+	tlsConfig := tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		ServerName:   URL,
+	}
+
+	return tlsConfig, nil
 }
